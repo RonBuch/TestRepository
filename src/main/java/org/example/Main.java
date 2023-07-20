6f3a0d5c0b3451f303c1f76710fd554254d5e97c
Mike <73053764+RonBuch@users.noreply.github.com>
2023-07-20 11:28:02 +0700

    Добавлено несколько функций

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,28 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        hello("World");
+        hello("user");
+        hello("Mike");
+
+        double l = 4;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со стронами " + a + " и " + b + " = "+ area(a, b));
+
+    }
+
+    public static void hello(String somedoy){
+        System.out.println("Hello " + somedoy + "!");
+    }
+
+    public static double area (double len) {
+        return len * len;
+    }
+
+    public static double area (double a, double b) {
+        return a * b;
     }
 }
